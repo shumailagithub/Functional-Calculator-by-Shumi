f66a7744f5f3d9597b0b25e45f7c82fa84abaad5
Shumaila <163028638+shumailagithub@users.noreply.github.com>
2024-10-23 08:58:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let ScreenD = document.getElementById("Screen");
+offFunc();
+function offFunc() {
+    ScreenD.style.backgroundColor = "black";
+}
+function onFunc() {
+    ScreenD.style.backgroundColor = "white";
+}
---
+++ app.ts
@@ -0,0 +1,10 @@
+let ScreenD: any = document.getElementById("Screen")
+
+offFunc()
+ 
+function offFunc(){
+    ScreenD.style.backgroundColor = "black"
+}function onFunc() {
+    ScreenD.style.backgroundColor = "white"
+}
+
